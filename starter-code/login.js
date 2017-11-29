9a7f347ff1c663c9a17fb1ac6b72885dc6e43ca1
Eva <evaliisagalen@gmail.com>
2017-11-29 08:30:50 -0800

    with my js codes

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,13 @@
 console.log("login.js loaded");
+
+var userLogin = {userName: "admin", password: "abcd1234"};
+
+for (i = 0; i < 3; i++) {
+  var userPassword = prompt("Enter password for " + userLogin.userName);
+  if (userPassword == userLogin.password) {
+    alert("It's a match!");
+    break;
+  } else {
+    alert("Try again!");
+  };
+};
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,29 @@
 console.log("security_questions.js loaded");
+security_questions = [
+  {
+    question: "What town were you born in?",
+    expectedAnswer: "San Francisco"
+  },
+  {
+    question: "What is your pet's name?",
+    expectedAnswer: "Milo"
+  },
+  {
+    question: "What year were you born?",
+    expectedAnswer: 1985
+  }
+]
+
+
+var answer = ""
+
+
+for (var i = 0; i < security_questions.length; i++) {
+  answer = prompt(security_questions[i].question);
+  if (answer == security_questions[i].expectedAnswer) {
+    alert("It's a match!");
+    break;
+  } else {
+    alert("Try again!");
+  };
+};
