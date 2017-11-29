8ac74adbb0c2f3b30a5104a0e9b3c77cfb48d994
Uyen Tran Thi Thao <LillianTran@Uyens-MacBook-Air-4.local>
2017-11-29 12:02:58 -0800

    myhomework

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,24 @@
 console.log("security_questions.js loaded");
+var securityQuestions = [
+	{
+		question: "What was your first pet's name?",expectedAnswer: "miki"
+	},
+	{
+		question: "what is your mother name?",expectedAnswer: "Alice"
+	},
+	{
+		question: "What is your first job?",expectedAnswer: "Architecture"
+	}
+]
+
+var userAnswer = "";
+
+
+for (var i=0; i < securityQuestions.length; i++){
+	userAnswer = prompt(securityQuestions[i].question);
+	if (userAnswer !== securityQuestions[i].expectedAnswer){
+		alert("Incorrect security question!");
+    
+		break;
+	}
+}
