c6bc43277b653ffbf927a5b6ee72080c833f0d22
tinaparija <tinaparija@gmail.com>
2017-11-28 20:28:29 -0800

    control flow homework solutions

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,29 @@
 console.log("security_questions.js loaded");
+
+let securityQuestions = [
+{question: "What's your favorite color?",
+expectedAnswer:"Teal" 
+},
+
+{question: "Where were you born?",
+expectedAnswer: "Cuttack"
+},
+
+{question: "Who was your first roommate?",
+expectedAnswer: "Natasha"
+}];
+
+for ( i = 0; i < securityQuestions.length; i++){
+	var input = prompt(securityQuestions[i].question);
+	if (input !== securityQuestions[i].expectedAnswer){
+		alert("That's not correct.");
+		break;
+	};
+};
+
+// numIterations ensures this while loop doesn't go on forever
+var numIterations = 0;
+while (true && numIterations < 100){
+  console.log("still going!");
+  numIterations++;
+}
