a392d0a6791d0764f35a124baccb0979f78f8f10
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-11-28 17:15:46 -0800

    security questions added

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,39 @@
 console.log("security_questions.js loaded");
+
+// numIterations ensures this while loop doesn't go on forever
+var numIterations = 0;
+while (true && numIterations < 100){
+  console.log("still going!");
+  numIterations++;
+}
+
+secrityQuestions = [
+  {
+    question: "What was your the color of your first car?",
+    expectedAnswer: "red"
+  },
+  {
+    question: "In what city were you born?",
+    expectedAnswer: "Fort Wayne"
+  },
+  {
+    question: "What is your favorite winter sport?",
+    expectedAnswer: "skiing"
+  }
+];
+
+  for (var i = 0; i < secrityQuestions.length; i++) {
+    let answer = prompt(secrityQuestions[i].question);
+    if (answer !== secrityQuestions[i].expectedAnswer) {
+      alert("Wrong answer!");
+      break; 
+      }
+    }
+
+
+
+
+
+
+
+
